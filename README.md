e0605dc92881adfaf67f652334293ab00f22a9aa
Alejandro Javier <40289632+DemiurgeApeiron@users.noreply.github.com>
2021-12-20 23:31:58 -0600

    Global Indicators DataSet inclusion & Code Update

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # GlobalWarming_DataAnalysis
+
+---
+
+DataSets:
+
+-   2020 World Development Indicators Collected By Manch Hui
+    https://www.kaggle.com/manchunhui/world-development-indicators
+-   Climate Change: Earth Surface Temperature Data Collected By Berkeley Earth
+    https://www.kaggle.com/berkeleyearth/climate-change-earth-surface-temperature-data
--- SQL_Python_CW.py
+++ SQL_Python_CW.py
@@ -26,19 +26,33 @@ def makeConnection():
 
 
 def SQL_Querry(cursor):
-    queryGetName = f"SELECT dt, LandAverageTemperature, LandAverageTemperatureUncertainty FROM GlobalWarming.global_temperatures WHERE dt <= 2020"
-    cursor.execute(queryGetName)
-    data = cursor.fetchall()
-    return data
+    queryGetGlobalTemperatures = f"SELECT dt, LandAverageTemperature, LandAverageTemperatureUncertainty FROM GlobalWarming.global_temperatures"
+    cursor.execute(queryGetGlobalTemperatures)
+    dataGlobalTemperatures = cursor.fetchall()
+    queryGetGlobalIndicators = f"SELECT Year, Value, CountryName, IndicatorName FROM GlobalWarming.world_indicators WHERE IndicatorCode LIKE '%CO2E.KT%' ORDER BY Year;"
+    cursor.execute(queryGetGlobalIndicators)
+    dataGlobalIndicators = cursor.fetchall()
+
+    return dataGlobalTemperatures, dataGlobalIndicators
 
 
-def data_processing(list):
-    df = pd.DataFrame(list, columns=["Date", "AverageTemperature", "LandAverageTemperatureUncertainty"])
-    df["Date"] = pd.to_datetime(df["Date"])
-    print(df.info())
-    print(df.describe())
-    print(df.head())
-    return df
+def data_processing(dataGlobalTemperatures, dataGlobalIndicators):
+    dfGlobalTemperatures = pd.DataFrame(dataGlobalTemperatures, columns=["Date", "AverageTemperature", "LandAverageTemperatureUncertainty"])
+    dfGlobalIndicators = pd.DataFrame(dataGlobalIndicators, columns=["Date", "Value", "CountryName", "IndicatorName"])
+
+    dfGlobalTemperatures["Date"] = pd.to_datetime(dfGlobalTemperatures["Date"])
+    dfGlobalIndicators["Date"] = pd.to_datetime(dfGlobalIndicators["Date"])
+
+    dfGlobalTemperatures = dfGlobalTemperatures.dropna()
+
+    print(dfGlobalTemperatures.info())
+    print(dfGlobalTemperatures.describe())
+    print(dfGlobalTemperatures.head())
+
+    print(dfGlobalIndicators.info())
+    print(dfGlobalIndicators.describe())
+    print(dfGlobalIndicators.head())
+    return dfGlobalTemperatures, dfGlobalIndicators
 
 
 def dataDisplay(DataFrameProcessed):
@@ -51,12 +65,12 @@ def dataDisplay(DataFrameProcessed):
     plt.show()
 
 
-def DataModel(DataFrameProcessed):
-    DataFrameProcessed["Year"] = DataFrameProcessed["Date"].dt.year
-    DataFrameProcessed["Month"] = DataFrameProcessed["Date"].dt.month
-    DataFrameProcessed["Day"] = DataFrameProcessed["Date"].dt.day
-    x = DataFrameProcessed[["Year", "Month", "Day"]].to_numpy()
-    y = DataFrameProcessed["AverageTemperature"].to_numpy()
+def DataModel(dfGlobalTemperatures, dfGlobalIndicators):
+    dfGlobalTemperatures["Year"] = dfGlobalTemperatures["Date"].dt.year
+    dfGlobalTemperatures["Month"] = dfGlobalTemperatures["Date"].dt.month
+    dfGlobalTemperatures["Day"] = dfGlobalTemperatures["Date"].dt.day
+    x = dfGlobalTemperatures[["Year", "Month", "Day"]].to_numpy()
+    y = dfGlobalTemperatures["AverageTemperature"].to_numpy()
     X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.20)
     print(X_train)
     # scaler = StandardScaler()
@@ -74,13 +88,13 @@ def DataModel(DataFrameProcessed):
     model.compile(optimizer=opt, loss="mse")
     # learning rate schededuler (variable lr)
     def schedule(epoch, lr):
-        if epoch >= 50:
+        if epoch >= 10:
             print(lr)
-            return lr - lr / 50
+            return lr - lr / 10
         return 0.01
 
     scheduler = tf.keras.callbacks.LearningRateScheduler(schedule)
-    r = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=70, callbacks=[scheduler])
+    r = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=20, callbacks=[scheduler])
 
     print(f"Train score: {model.evaluate(X_train, y_train)}")
     print(f"Test score: {model.evaluate(X_test, y_test)}")
@@ -95,29 +109,43 @@ def DataModel(DataFrameProcessed):
     print(f"Raíz de la desviación media al cuadrado: {rdmc}")
     plt.show()
 
-    X_year = 2020
+    X_year = 2017
+    X_Year_Month_Day = []
     futureYears = []
     for i in range(50):
         X_year += 1
-        futureYears.append([X_year, 1, 1])
+        X_Month = 1
+        # for j in range(11):
+        # X_Month += 1
+        X_Day = 1
+        # for k in range(27):
+        # X_Day += 1
+        futureYears.append([X_year, X_Month, X_Day])
+        X_Year_Month_Day.append(f"{X_year}-{X_Month}-{X_Day}")
 
     futureYears = np.array(futureYears)
+    df = pd.DataFrame(X_Year_Month_Day, columns=["Date"])
+    df["Date"] = pd.to_datetime(df["Date"])
     # futureYears = scaler.fit_transform(futureYears.reshape(-1, 1))
     yhatFutureYears = model.predict(futureYears).flatten()
-    print(futureYears[0], yhatFutureYears)
-    print(len(futureYears[0]), len(yhatFutureYears))
-
-    sns.lineplot(x=np.array([2020 + x for x in range(50)]), y=yhatFutureYears)
+    # x=np.array([2020 + x for x in range(50)])
+    df["AverageTemperature"] = pd.DataFrame(yhatFutureYears, columns=["AverageTemperature"])
+    df = df.set_index("Date")
+    ndf = dfGlobalTemperatures[["Date", "AverageTemperature"]]
+    ndf = ndf.groupby(pd.Grouper(key="Date", freq="Y")).mean()
+    df = pd.concat([df, ndf])
+    print(df.head())
+    sns.lineplot(data=df)
     plt.show()
 
 
 def main():
     cnx = makeConnection()
     cursor = cnx.cursor()
-    list = SQL_Querry(cursor)
-    DataFrameProcessed = data_processing(list)
-    dataDisplay(DataFrameProcessed)
-    DataModel(DataFrameProcessed)
+    dataGlobalTemperatures, dataGlobalIndicators = SQL_Querry(cursor)
+    dfGlobalTemperatures, dfGlobalIndicators = data_processing(dataGlobalTemperatures, dataGlobalIndicators)
+    dataDisplay(dfGlobalTemperatures)
+    DataModel(dfGlobalTemperatures, dfGlobalIndicators)
     # cnx.commit()
     cnx.close()
 
